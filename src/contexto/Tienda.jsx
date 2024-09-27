b257407353a4596c091bca095e43aa674a325e5a
landher <landher__@hotmail.com>
2024-09-27 15:43:12 -0500

    aplicando axios

--- src/contexto/Tienda.jsx
+++ src/contexto/Tienda.jsx
@@ -1,13 +1,14 @@
-import { createContext, useState } from "react";
-import { areas } from '../datos'
+import { createContext, useEffect, useState } from "react";
+// import { areas } from '../datos'
+import axios from "axios";
 export const ContextoTienda = createContext();
 
 export function ProveedorTienda({children}) {
-    const [listaAreas, setListaAreas] = useState(areas);
-    const [seleccion, setSeleccion] = useState(areas[0]);
+    const [listaAreas, setListaAreas] = useState([]);
+    const [seleccion, setSeleccion] = useState({});
     const [contenidoCarrito, setContenidoCarrito] = useState([]);
     const manejadorClickArea = id => {
-        const areaSeleccionada = areas.filter( area => area.id === id)[0];
+        const areaSeleccionada = listaAreas.filter( area => area.id === id)[0];
         setSeleccion(areaSeleccionada);
     }
     const manejadorClickCurso = curso => {
@@ -20,6 +21,14 @@ export function ProveedorTienda({children}) {
         setContenidoCarrito(nuevoContenido);
     }
     const manejadorClickVaciar = () => setContenidoCarrito([]);
+    useEffect(() => {
+        async function recuperarAreas() {
+            const {data} = await axios('http://127.0.0.1:8000/api/areas');
+            setListaAreas(data.data);
+            setSeleccion(data.data[0]);
+        }
+        recuperarAreas()
+    },[]);
     return (
         <ContextoTienda.Provider value={{
                 listaAreas,
